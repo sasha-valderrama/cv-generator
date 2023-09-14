953aca42c230a2584fb24462630fa47a5c1f8244
sasha-valderrama <103856472+sasha-valderrama@users.noreply.github.com>
2023-09-14 14:58:24 +0200

    Add font Alegreya, Layout of mainpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom'
 import './App.css'
+import Root from './components/Root'
+import Instructions from './components/Instructions'
+import Form from './components/Form'
+
+const appRouter = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Root />}>
+      <Route index element={<Instructions />} />
+      <Route path="/form" element={<Form />} />
+    </Route>
+  )
+)
 
 function App() {
-
-
-  return (
-    <>
-     
-    </>
-  )
+  return <RouterProvider router={appRouter} />
 }
 
 export default App
